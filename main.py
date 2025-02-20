bc2f8283798d56236c0659fdc2649de7998ef0ec
John-Leeman <johndleeman@gmail.com>
2025-02-19 19:15:02 -0800

    rotate player left/right

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
